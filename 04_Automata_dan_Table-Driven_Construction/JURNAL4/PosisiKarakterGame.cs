d9deced0d43a6a786580137c807aa1003acef1d4
Ramdaniiiii <168392836+Ramdaniiiii@users.noreply.github.com>
2025-03-11 00:53:03 +0700

    Mengumpulkan Jurnal

---
+++ 04_Automata_dan_Table-Driven_Construction/JURNAL4/PosisiKarakterGame.cs
@@ -0,0 +1,40 @@
+﻿class PosisiKarakterGame
+{
+    public enum Posisi { Berdiri, Jongkok, Tengkurap, Terbang }
+    private Posisi posisiSaatIni;
+
+    public PosisiKarakterGame()
+    {
+        posisiSaatIni = Posisi.Berdiri;
+    }
+
+    public void TombolW()
+    {
+        if (posisiSaatIni == Posisi.Berdiri)
+        {
+            posisiSaatIni = Posisi.Terbang;
+            Console.WriteLine("Posisi Take Off");
+        }
+        else if (posisiSaatIni == Posisi.Jongkok)
+        {
+            posisiSaatIni = Posisi.Berdiri;
+            Console.WriteLine("Posisi Standby");
+        }
+    }
+
+    public void TombolS()
+    {
+        if (posisiSaatIni == Posisi.Terbang)
+        {
+            posisiSaatIni = Posisi.Jongkok;
+            Console.WriteLine("Posisi Landing");
+        }
+        else if (posisiSaatIni == Posisi.Berdiri)
+        {
+            posisiSaatIni = Posisi.Jongkok;
+            Console.WriteLine("Posisi Istirahat");
+        }
+    }
+}
+
+// Program Utama
