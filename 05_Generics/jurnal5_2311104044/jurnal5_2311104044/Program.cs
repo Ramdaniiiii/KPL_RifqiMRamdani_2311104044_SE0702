6e291558d3840498108e7af8ddfe83e445a5e676
Ramdaniiiii <168392836+Ramdaniiiii@users.noreply.github.com>
2025-03-17 14:40:17 +0700

    upload tp dan jurnal modul 5

---
+++ 05_Generics/jurnal5_2311104044/jurnal5_2311104044/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace modul5_2311104044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Menjalankan Generic Method (Penjumlahan)
+            Penjumlahan<double> penjumlahan = new Penjumlahan<double>();
+            double hasil = penjumlahan.JumlahTigaAngka(23, 11, 10);
+            Console.WriteLine($"Hasil penjumlahan: {hasil}");
+
+            Console.WriteLine(); // Spasi antar output
+
+            // Menjalankan Generic Class (SimpleDataBase)
+            SimpleDataBase<int> database = new SimpleDataBase<int>();
+            database.AddNewData(23);
+            database.AddNewData(11);
+            database.AddNewData(10);
+            database.PrintAllData();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05_Generics/tpm5_2311104044/tpm5_2311104044/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace tpmodul5_2311104044
+{
+    class HaloGeneric
+    {
+        // Method generic yang menerima parameter tipe T
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+    class DataGeneric<T>
+    {
+        private T data;
+
+        // Konstruktor menerima parameter generic
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        // Method untuk mencetak data
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pemanggilan method generic dengan nama panggilan Ramdan
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser("Ramdan");
+
+            // Penggunaan class generic dengan NIM 2311104044
+            DataGeneric<string> dataNIM = new DataGeneric<string>("2311104044");
+            dataNIM.PrintData();
+
+            Console.ReadLine();
+        }
+    }
+}
