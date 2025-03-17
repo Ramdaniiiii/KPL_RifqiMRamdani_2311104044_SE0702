2b68fce47193aa42a8edcbb1f0418edb69334b34
Ramdaniiiii <168392836+Ramdaniiiii@users.noreply.github.com>
2025-03-17 15:28:57 +0700

    ngumpulin Tp

---
+++ 05_Generics/jurnal5_2311104044/jurnal5_2311104044/Penjumlahan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace modul5_2311104044
+{
+    class Penjumlahan<T>
+    {
+        public T JumlahTigaAngka(T angka1, T angka2, T angka3)
+        {
+            dynamic a = angka1;
+            dynamic b = angka2;
+            dynamic c = angka3;
+            return a + b + c;
+        }
+    }
+}
