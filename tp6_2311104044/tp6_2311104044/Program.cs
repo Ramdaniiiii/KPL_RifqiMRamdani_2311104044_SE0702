09fe6ddcd757a00e06a33843fd4cb03bf08e401a
Ramdaniiiii <168392836+Ramdaniiiii@users.noreply.github.com>
2025-03-23 16:57:38 +0700

    menambahkan gitignore

---
+++ tp6_2311104044/tp6_2311104044/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Ramdan");
+
+        video.PrintVideoDetails();
+
+        video.IncreasePlayCount(10000000);
+        video.PrintVideoDetails();
+
+        // Test overflow
+        for (int i = 0; i < 3; i++)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+
+        video.PrintVideoDetails();
+    }
+}
