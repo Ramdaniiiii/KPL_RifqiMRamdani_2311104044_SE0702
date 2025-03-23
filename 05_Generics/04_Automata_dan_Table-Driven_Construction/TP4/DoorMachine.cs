e6f2d5a6d649bcfbc8d311f000cab33dc134398b
Ramdaniiiii <168392836+Ramdaniiiii@users.noreply.github.com>
2025-03-23 16:38:13 +0700

    menambahkan gitignore dan jurnal

---
+++ 05_Generics/04_Automata_dan_Table-Driven_Construction/TP4/DoorMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class DoorMachine
+{
+    public enum State { Terkunci, Terbuka }
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void Toggle()
+    {
+        if (currentState == State.Terkunci)
+        {
+            currentState = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+    }
+}
---
+++ 05_Generics/04_Automata_dan_Table-Driven_Construction/TP4/KodePos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class KodePos
+{
+    private static Dictionary<string, string> kodePosDict = new Dictionary<string, string> {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijagra", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+
+    public static string GetKodePos(string kelurahan)
+    {
+        if (kodePosDict.ContainsKey(kelurahan))
+        {
+            return kodePosDict[kelurahan];
+        }
+        else
+        {
+            return "Kode Pos tidak ditemukan";
+        }
+    }
+}
